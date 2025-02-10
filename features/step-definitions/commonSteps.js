fda4e885124565c61c26cad0be15a1c8fd2fa1e4
Daniel <danielperez0232@gmail.com>
2025-02-10 21:28:40 +0100

    #5 Refeactoring of the code

--- features/step-definitions/commonSteps.js
+++ features/step-definitions/commonSteps.js
@@ -3,6 +3,10 @@ const { browser } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/login.page');
 const LoginOrangeHRPage = require('../pageobjects/loginOrangeHR.page');
 
+/**
+ * I have included all the common steps which are repeated in most logins tests, being filtered by a key word in the gherking in order to select the correct pageObject
+ */
+
 Given(/^I am on the "(.*)" page$/, async (page) => {
     if (page === 'login')  await LoginPage.open();
     if (page === 'orangehrm login') await LoginOrangeHRPage.open();
@@ -17,4 +21,9 @@ When(/^I enter (.*) and (.*) on "(.*)" page$/, async (username, password, page) 
 Then(/^I should see the "(.*)" page (.*)$/, async (page, url) => {
     const currentUrl = await browser.getUrl();
     if (page === 'dashboard')  await expect(currentUrl).toBe(url)
+});
+
+Then(/^I should see a error message saying (.*)$/, async (message) => {
+    await expect(IndexOrangeHRPage.errorMessage).toBeExisting();
+    await expect(IndexOrangeHRPage.errorMessage).toHaveText(expect.stringContaining(message));
 });
